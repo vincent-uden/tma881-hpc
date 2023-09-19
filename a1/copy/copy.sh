0c05b96673ed6c6ab1082fee794b47644c129700
Vincent Udén <vincentuden@gmail.com>
2023-09-19 15:20:36 +0200

    Copy script

---
+++ a1/copy/copy.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/bash
+
+# Copying to HDD -> 17.545 s
+# cp -r /usr/include/ ~/include
+# cp -r /usr/include/ ~/include
+# cp -r /usr/include/ ~/include
+# cp -r /usr/include/ ~/include
+# cp -r /usr/include/ ~/include
+# cp -r /usr/include/ ~/include
+# cp -r /usr/include/ ~/include
+# cp -r /usr/include/ ~/include
+# cp -r /usr/include/ ~/include
+# cp -r /usr/include/ ~/include
+
+# Copying to SSD -> 15.528
+cp -r /usr/include/ /run/mount/scratch/hpcuser156/include
+cp -r /usr/include/ /run/mount/scratch/hpcuser156/include
+cp -r /usr/include/ /run/mount/scratch/hpcuser156/include
+cp -r /usr/include/ /run/mount/scratch/hpcuser156/include
+cp -r /usr/include/ /run/mount/scratch/hpcuser156/include
+cp -r /usr/include/ /run/mount/scratch/hpcuser156/include
+cp -r /usr/include/ /run/mount/scratch/hpcuser156/include
+cp -r /usr/include/ /run/mount/scratch/hpcuser156/include
+cp -r /usr/include/ /run/mount/scratch/hpcuser156/include
+cp -r /usr/include/ /run/mount/scratch/hpcuser156/include
